98b3543bdc61bfe4a9ea8ee08baa6b373ad52bd3
Amir Tayabali <amir.tayabali1@gmail.com>
2024-07-08 23:13:23 +0100

    upgrade Module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -21,12 +21,19 @@ module "s3_bucket" {
   bucket_name                   = var.bucket_name
   object_lock_configuration     = var.object_lock_configuration
   s3_replication_enabled        = local.s3_replication_enabled
-  s3_replica_bucket_arn         = join("", module.s3_bucket_replication_target.*.bucket_arn)
+  s3_replica_bucket_arn         = join("", module.s3_bucket_replication_target[*].bucket_arn)
   s3_replication_rules          = local.s3_replication_rules
   privileged_principal_actions  = var.privileged_principal_actions
   privileged_principal_arns     = local.privileged_principal_arns
-  transfer_acceleration_enabled = true
+  transfer_acceleration_enabled = var.transfer_acceleration_enabled
   bucket_key_enabled            = var.bucket_key_enabled
+  source_policy_documents       = var.source_policy_documents
+  sse_algorithm                 = var.sse_algorithm
+  kms_master_key_arn            = var.kms_master_key_arn
+  block_public_acls             = var.block_public_acls
+  block_public_policy           = var.block_public_policy
+  ignore_public_acls            = var.ignore_public_acls
+  restrict_public_buckets       = var.restrict_public_buckets
 
   access_key_enabled      = var.access_key_enabled
   store_access_key_in_ssm = var.store_access_key_in_ssm
